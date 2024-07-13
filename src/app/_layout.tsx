6152f7cfd74c39323a3ccdf66305d4d6dae82d35
zync <chandankrishna288@gmail.com>
2024-07-13 13:50:43 +0530

    First Commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,48 @@
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 import "../global.css";
-import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
+import { Platform } from "react-native";
 
-export default function Layout() {
-  return <Slot />;
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  if (Platform.OS == "android") {
+    NavigationBar.setBackgroundColorAsync("#000000");
+  }
+  const [loaded] = useFonts({
+    "Poppins-Black": require("../../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar hidden />
+    </ThemeProvider>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,95 +1,208 @@
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
+import React, { useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import * as Clipboard from "expo-clipboard";
 
-export default function Page() {
+const Index = () => {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [responseBlock, setResponseBlock] = useState([]);
+  const inputRef = useRef(null);
+
+  async function handleSubmit() {
+    try {
+      if (query.length >= 2) {
+        Keyboard.dismiss();
+        inputRef.current.clear();
+        setLoading(true);
+        const response = await fetch("https://zyncai.vercel.app/api", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ query }),
+        });
+        const res = await response.json();
+        setError(false);
+        setLoading(false);
+        setResponseBlock((prev) => [
+          ...prev,
+          {
+            query: query.trimEnd().trimStart(),
+            res,
+          },
+        ]);
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+      console.log("Error", error);
+    }
+  }
+
+  // border-2 border-red-500
+
   return (
-    <View className="flex flex-1">
-      <Header />
-      <Content />
-      <Footer />
-    </View>
-  );
-}
-
-function Content() {
-  return (
-    <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Welcome to Project ACME
-            </Text>
-            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on amce. Explore our services now.
-            </Text>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
-            </View>
+    <SafeAreaView>
+      <View className="h-full">
+        <View className="justify-between items-center py-2 px-3 flex-row fixed top-0 left-0 right-0">
+          <Text className="text-white font-psemibold text-3xl">
+            Zync <Text className="text-green-500">AI</Text>
+          </Text>
+          <View className="flex-row gap-3 items-center">
+            <FontAwesome
+              name="rotate-left"
+              color={"white"}
+              size={26}
+              onPress={() => {
+                setResponseBlock([]);
+                setQuery("");
+              }}
+            />
+            <Ionicons name="menu-outline" color={"white"} size={40} />
           </View>
         </View>
-      </View>
-    </View>
-  );
-}
-
-function Header() {
-  const { top } = useSafeAreaInsets();
-  return (
-    <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
-        </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
+        <ScrollView className="h-full">
+          <View className="py-2">
+            {!loading && responseBlock.length == 0 && (
+              <View className="px-2 mt-5">
+                <Text className="text-5xl text-[#22c55e] font-psemibold">
+                  Hello,
+                </Text>
+                <Text className="text-5xl text-[#444746] font-psemibold">
+                  How can I help you today?
+                </Text>
+              </View>
+            )}
+            {responseBlock.map((singleResponse, i) => (
+              <View className="px-2 relative mb-4" key={i}>
+                <View
+                  className="place-content-end mb-4"
+                  style={{ alignSelf: "flex-end" }}
+                >
+                  <Text
+                    style={{
+                      alignSelf: "flex-start",
+                      textAlignVertical: "center",
+                      borderRadius: 24,
+                      overflow: "hidden",
+                    }}
+                    className="text-white font-pmedium text-lg bg-[#1a1a1a] p-3 rounded-3xl"
+                  >
+                    {singleResponse.query}
+                  </Text>
+                </View>
+                <View>
+                  <Text
+                    className="text-white font-pmedium text-lg bg-[#1a1a1a] pt-5 px-3 rounded-3xl"
+                    style={{
+                      alignSelf: "flex-start",
+                      textAlignVertical: "center",
+                      borderRadius: 24,
+                      overflow: "hidden",
+                    }}
+                  >
+                    {singleResponse.res}
+                  </Text>
+                </View>
+              </View>
+            ))}
+            {loading && (
+              <View className="w-full h-[300px] bg-[#1a1a1a] rounded-3xl animate-pulse"></View>
+            )}
+          </View>
+        </ScrollView>
+        {!loading && responseBlock.length == 0 && (
+          <View className="gap-4 mb-6">
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View className="flex-row gap-4">
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  className="flex-1 bg-[#1a1a1a] rounded-3xl p-5"
+                  onPress={() => setQuery("Create a Workout Plan for me")}
+                >
+                  <Text className="font-pmedium text-xl color-white w-[130px]">
+                    Create a Workout Plan for me
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  className="flex-1 bg-[#1a1a1a] rounded-3xl p-5"
+                  onPress={() => setQuery("Tell me 5 interesting Facts")}
+                >
+                  <Text className="font-pmedium text-xl color-white w-[130px]">
+                    Tell me 5 interesting Facts
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  className="flex-1 bg-[#1a1a1a] rounded-3xl p-5"
+                  onPress={() => setQuery("Give information about neurallink")}
+                >
+                  <Text className="font-pmedium text-xl color-white w-[130px]">
+                    Give info about neurallink
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  className="flex-1 bg-[#1a1a1a] rounded-3xl p-5"
+                  onPress={() =>
+                    setQuery("What is the temperature of the Sun?")
+                  }
+                >
+                  <Text className="font-pmedium text-xl color-white w-[130px]">
+                    What is the temperature of the Sun?
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </ScrollView>
+          </View>
+        )}
+        <View className="w-ful flex-row fixed bottom-0 right-0 left-0 my-3 items-center gap-x-3">
+          <TextInput
+            textAlignVertical="center"
+            value={query}
+            placeholder="Ask Gemini and LLama 3"
+            className="rounded-3xl px-5 pt-3 h-[60px] pb-2 color-white font-pmedium flex-1 bg-[#1a1a1a]"
+            onChangeText={(input: string) => setQuery(input)}
+            placeholderTextColor={"#ffffff"}
+            cursorColor={"#ffffff"}
+            ref={inputRef}
+            // autoFocus
+            multiline
+          />
+          <TouchableOpacity
+            disabled={loading}
+            className={`bg-white rounded-full items-center justify-center p-[8px]`}
+            onPress={handleSubmit}
           >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
+            <Ionicons name="arrow-up-outline" color={"#000000"} size={30} />
+          </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
-}
+};
 
-function Footer() {
-  const { bottom } = useSafeAreaInsets();
-  return (
-    <View
-      className="flex shrink-0 bg-gray-100 native:hidden"
-      style={{ paddingBottom: bottom }}
-    >
-      <View className="py-6 flex-1 items-start px-4 md:px-6 ">
-        <Text className={"text-center text-gray-700"}>
-          © {new Date().getFullYear()} Me
-        </Text>
-      </View>
-    </View>
-  );
-}
+export default Index;
+
+// <TouchableOpacity>
+//   <Text
+//     className="text-white p-5 rounded-2xl"
+//     onPress={() => {
+//       setResponse("");
+//       setQuery("");
+//     }}
+//   >
+//     <FontAwesome name="ban" size={40} />
+//   </Text>
+// </TouchableOpacity>
